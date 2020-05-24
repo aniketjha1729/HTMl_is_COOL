8da3536f81976fb3af6df0ec9f1da7d4355a070b
aniketjha1729 <kumaraniket1729@gmail.com>
2020-05-25 04:35:11 +0530

    first

---
+++ module/main.js
@@ -0,0 +1,82 @@
+var phi=document.getElementById("phi")
+var attempt = document.getElementById("attempt")
+var hoh = document.getElementById("hoh")
+var phif = document.getElementById("phif")
+var alphal = document.getElementById("alphal")
+var output = document.getElementById("output")
+var imagesource = document.getElementById("imagesource")
+
+var Thoh = 18924.78042
+var Phif = 100.9191688
+var AlphaL = 79.08083116
+var k=""
+
+var omega1rad = 0.00118555196
+var omaga2rad = 0.00007293193628
+var phifrad = 1.761371774
+var phiirad=""
+
+window.onload = function () {
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    var img = document.getElementById("imagesource");
+    ctx.drawImage(img, 0, 0, 400, 360);
+    ctx.beginPath();
+    ctx.arc(100, 75, 50, 0, 2 * Math.PI);
+    ctx.strokeStyle = "white";
+    ctx.stroke();
+}
+
+
+window.fphi=(e)=>{  
+    if(e.value=="5"){
+        phiirad = 0.0872664626
+    }else if(e.value=="10"){
+        phiirad = 0.1745329252
+    } else if (e.value == "50") {
+        phiirad = 0.872664626
+    } else if (e.value == "90") {
+        phiirad = 1.570796327
+    } else if (e.value == "130") {
+        phiirad = 2.268928028
+    } else if (e.value == "150") {
+        phiirad = 2.617993878
+    } else if (e.value == "180") {
+        phiirad = 3.141592654
+    } else if (e.value == "220") {
+        phiirad = 3.839724354
+    } else if (e.value == "250") {
+        phiirad = 4.36332313
+    } else if (e.value == "300") {
+        phiirad = 5.235987756
+    } else if (e.value == "360") {
+        phiirad = 6.283185307
+    }
+    $("#attempt").prop("selectedIndex", 0);
+    phi.style.setProperty('border', 'initial')
+    attempt.style.border = "solid Red";
+    hoh.value=Thoh;
+    hoh.style.setProperty('border', 'initial')
+    phif.value=Phif;
+    phif.style.setProperty('border', 'initial')
+    alphal.value=AlphaL;
+    alphal.style.setProperty('border', 'initial')
+    output.innerHTML = "";
+}
+
+window.fattmp=(e)=>{
+    k=attempt.value
+    attempt.style.setProperty('border', 'initial')
+    output.innerHTML="";
+}
+
+window.showResult=(e)=>{
+    console.log("phirad phifrad attempt omega1rad omega2rad",phiirad,phifrad,k,omega1rad,omaga2rad)
+    var numerator = (phiirad - phifrad)+(2*k*3.14)
+    var deno = omega1rad - omaga2rad
+    console.log("phidiff",phiirad - phifrad)
+    console.log("deno",deno)
+    var ans=(numerator/deno)/60
+    output.innerHTML=`Well Done! Get Ready to fire your engine in ${ans} minutes`;
+}
+
