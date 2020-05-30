45ca449f0be2d985509b9f63b4258709080205f2
aniketjha1729 <kumaraniket1729@gmail.com>
2020-05-30 19:05:00 +0530

    final staging

--- module/main.js
+++ module/main.js
@@ -10,6 +10,7 @@ var canvas1=document.getElementById("myCanvas")
 var canvas2=document.getElementById("myCanvas1")
 var innertemp = document.getElementById("innertemp")
 var outertemp = document.getElementById("outertemp")
+var redcross = document.getElementById("redcross")
 // var imagesource = document.getElementById("imagesource")
 
 var Thoh = 18924.78042
@@ -21,7 +22,7 @@ var omega1rad = 0.00118555196
 var omaga2rad = 0.00007293193628
 var phifrad = 1.761371774
 var phiirad=""
-
+var flag=0;
 
 var ctx1 = canvas1.getContext("2d");
 var radius=130
@@ -41,6 +42,7 @@ window.fphi=(e)=>{
         inner.style.display = "block";
         outertemp.style.display = "none";
         outer.style.display = "block"
+        redcross.style.display = "none"
         canvas2.style.display="none"}
     if(e.value=="5"){
         phiirad = 0.0872664626
@@ -83,6 +85,7 @@ window.fattmp=(e)=>{
     if (buttonVal == 1) {
         canvas2.style.display = "none"
     }
+    flag=1;
     k=attempt.value
     attempt.style.setProperty('border', 'initial')
     output.innerHTML="";
@@ -90,36 +93,40 @@ window.fattmp=(e)=>{
 }
 
 window.showResult=(e)=>{
-    buttonVal=1;
-    canvas2.style.display = "block"
-    
-    innertemp.style.display="block";
-    inner.style.display="none";
-    outertemp.style.display="block";
-    outer.style.display="none"
-    var ctx2 = canvas2.getContext("2d");
-    ctx2.beginPath();
-    ctx2.arc(215, 270, 190, 0, 1. * Math.PI, true)
-    
-    ctx2.strokeStyle = "#FFFF00";
-    ctx2.lineWidth = 3;
-    ctx2.setLineDash([5, 3]);
-    ctx2.stroke();
-    radius = radius + 122
-    console.log("phirad phifrad attempt omega1rad omega2rad",phiirad,phifrad,k,omega1rad,omaga2rad)
-    var numerator = (phiirad - phifrad)+(2*k*3.14)
-    var deno = omega1rad - omaga2rad
-    console.log("phidiff",phiirad - phifrad)
-    console.log("deno",deno)
-    var ans=parseFloat((numerator/deno)/60).toFixed(2)
-    if(ans<=60 && ans>0){
-        output.innerHTML=`Well Done! Get Ready to fire your engine in ${ans} minutes.`;
-    }else if(ans>60){
-        output.innerHTML = `Oops! Its gone a take more than an hour ${ans} mins to inginte your engine for docking,
-        Tweek some values to start sooner.`;
-    }else if(ans<0){
-        output.innerHTML = `Oh No!! Unfortunately you missed the chance to start docking at very 1st attempt. Please try again.`;
+    if(flag==1){
+        buttonVal=1;
+        
 
+        radius = radius + 122
+        console.log("phirad phifrad attempt omega1rad omega2rad",phiirad,phifrad,k,omega1rad,omaga2rad)
+        var numerator = (phiirad - phifrad)+(2*k*3.14)
+        var deno = omega1rad - omaga2rad
+        console.log("phidiff",phiirad - phifrad)
+        console.log("deno",deno)
+        var ans=parseFloat((numerator/deno)/60).toFixed(2)
+        if(ans<=60 && ans>0){
+            canvas2.style.display = "block"
+            innertemp.style.display = "block";
+            inner.style.display = "none";
+            outertemp.style.display = "block";
+            outer.style.display = "none"
+            redcross.style.display = "block"
+
+            var ctx2 = canvas2.getContext("2d");
+            ctx2.beginPath();
+            ctx2.arc(215, 270, 190, 0, 1. * Math.PI, true)
+            ctx2.strokeStyle = "#FFFF00";
+            ctx2.lineWidth = 3;
+            ctx2.setLineDash([5, 3]);
+            ctx2.stroke();
+            output.innerHTML=`Well Done! Get Ready to fire your engine in ${ans} min to meet the satelite in GEO at the marked point.`;
+        }else if(ans>60){
+            output.innerHTML = `Oops! Its Gonna take more than an hour i.e around ${ans} mins to ignite your engine for docking.
+            Change the attempt number to start sooner.`;
+        }else if(ans<0){
+            output.innerHTML = `Oh No!! Unfortunately you missed the chance to start docking at very 1st attempt. Please try again.`;
+        }
+        flag=0;
     }
 }
 
