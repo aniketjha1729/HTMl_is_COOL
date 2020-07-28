90ba237c0fbfcfedaedec4f2c518ae14581c9c7b
aniketjha1729 <kumaraniket1729@gmail.com>
2020-07-28 15:55:02 +0530

    Base

--- jsgame/src/App.js
+++ jsgame/src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import Level1 from './Components/IntroToGame/Level1';
 import Level2 from './Components/IntroToGame/Level2';
 import Level7 from "./Components/ForLoop/Level7"
+import Welcome from './Components/Base/Welcome';
 
 class App extends Component {
   render() {
     return (
       // <Level1/>
       // <Level2/>
-      <Level7/>
+      // <Level7/>
+      <Welcome/>
       
     );
   }
